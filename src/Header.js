87aaf2dc2ea8937973355e1744207f16ed4f1e2c
My Tran <myytran28@gmail.com>
2021-02-22 09:56:34 -0500

    add header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className="header-container">
+            <h2>BeerCycle</h2>
+            
+        </div>
+    );
+}
+
+export default Header;
